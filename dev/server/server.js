2cc74175cf8e2228e0f1642e48df0710e6290ae3
Regina Garcia <Regina_Garcia@intuit.com>
2014-07-03 21:03:36 -0700

    first commit

--- dev/server/server.js
+++ dev/server/server.js
@@ -2,6 +2,7 @@ var restify     =   require('restify');
 var mongojs     =   require('mongojs');
 var morgan      =   require('morgan');
 var db          =   mongojs('bucketlistapp', ['appUsers','bucketLists']);
+
 var server      =   restify.createServer();
 
 server.use(restify.acceptParser(server.acceptable));
--- prod/server/server.js
+++ prod/server/server.js
@@ -1,8 +1,10 @@
 var restify     =   require('restify');
 var mongojs     =   require('mongojs');
 var morgan      =   require('morgan');
-var db          =   mongojs('bucketlistapp', ['appUsers','bucketLists']);
+var db          =   mongojs('mongodb://admin:test@ds037617.mongolab.com:37617/bucketlistapp', ['users','bucketLists']);
+//var db          =   mongojs('mongodb://admin:admin123@dbh36.mongolab.com:27367/bucketlistapp', ['users','bucketLists']);
 var server      =   restify.createServer();
+
 
 server.use(restify.acceptParser(server.acceptable));
 server.use(restify.queryParser());
